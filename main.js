6854ebfc68a19d23186530cab56105323d5b94b4
edeniie <91111147+edeniie@users.noreply.github.com>
2021-10-30 23:15:50 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+// const toggleBtn = document.querySelector(".navbar-toggleBtn");
+// const menu = document.querySelector(".navbar-menu");
+// const icons = document.querySelector('.navbar-links');
+
+// toggleBtn.addEventListener('click', () => {
+//     menu.classList.toggle('active');
+//     icons.classList.toggle('active');
+// });
+
+
+const toggleBtn = document.querySelector('.navbar-toggleBtn');
+const menu = document.querySelector('.navbar-menu');
+const icons = document.querySelector('.navbar-links');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+})
